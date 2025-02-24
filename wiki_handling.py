34b9d970eedc20ffa0cfe291ce52877ddfbad4d9
Lee Roy Stevenson <leroysheep@gmail.com>
2025-02-24 23:07:41 +0100

    saver coding

--- wiki_handling.py
+++ wiki_handling.py
@@ -1,1 +1,17 @@
+from PyQt6.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton
+import sys
 
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+def main():
+    app = QApplication(sys.argv)
+    window = QMainWindow()
+
+    
+    window.show()
+    app.exec()
+
+if __name__ == "__main__":
+    main()
