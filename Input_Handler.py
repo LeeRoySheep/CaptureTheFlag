584760c064ef85f5f8b46863a735b7442ce9b23d
Lee Roy Stevenson <leroysheep@gmail.com>
2025-02-26 15:10:51 +0100

    hangups

--- Input_Handler.py
+++ Input_Handler.py
@@ -25,7 +25,7 @@ def check_int_input(min=None,inclusive_min=True,max=None,inclusive_max=True):
                     + f' bis {max} ausgeschlossen ein: '
                 )
                 if user_int.isdecimal():
-                    if max >= int(user_int) >= min:
+                    if max > int(user_int) >= min:
                         return int(user_int)
                     else:
                         print("\n!Falsche Eingabe!\n")
@@ -34,27 +34,58 @@ def check_int_input(min=None,inclusive_min=True,max=None,inclusive_max=True):
                     f'Bitte geben Sie eine ganze Zahl ausgeschlossen {min}'
                     + f' bis {max} eingeschlossen an: '
                     )
+                if user_int.isdecimal():
+                    if max >= int(user_int) > min:
+                        return int(user_int)
+                    else:
+                        print("\n!Falsche Eingabe!\n")
             else:
                 user_int = input(
                     f'Geben Sie ein Zahl von ausgeschlossen {min} bis'
                     + f' {max} ausgeschlossen an: '
-                    ) 
+                    )
+                if user_int.isdecimal():
+                    if max > int(user_int) > min:
+                        return int(user_int)
+                    else:
+                        print("\n!Falsche Eingabe!\n")
         elif min != None:
             if inclusive_min:
                 user_int = input('Bitte geben Sie eine ganze Zahl größer gleich {min} ein: ')
+                if user_int.isdecimal():
+                    if int(user_int) >= min:
+                        return int(user_int)
+                    else:
+                        print("\n!Falsche Eingabe!\n")
             else:
                 user_int = input('Bitte geben Sie eine ganze Zahl echt größer {min} ein: ')
+                if user_int.isdecimal():
+                    if int(user_int) > min:
+                        return int(user_int)
+                    else:
+                        print("\n!Falsche Eingabe!\n")
         elif max != None:
             if inclusive_max:
                 user_int = input(f'Bitte geben Sie eine ganze Zahl kleiner oder gleich {max} ein: ')
+                if user_int.isdecimal():
+                    if max >= int(user_int):
+                        return int(user_int)
+                    else:
+                        print("\n!Falsche Eingabe!\n")
             else:
-                user_int = input(f'Bitte geben Sie eine ganze Zahl kleiner {max} ein: ')
+                user_int = input(f'Bitte geben Sie eine ganze Zahl echt kleiner {max} ein: ')
+                if user_int.isdecimal():
+                    if max > int(user_int):
+                        return int(user_int)
+                    else:
+                        print("\n!Falsche Eingabe!\n")
         else:
             user_int = input(f'Bitte geben Sie eine beliebige ganze Zahl an: ')
+            if user_int.isdecimal():
+                return int(user_int)
+            else:
+                print("\n!Falsche Eingabe!\n")
 
-        if user_int.isdecimal():
-            return int(user_int)
-        else:
-            print("\n!Falsche Eingabe!\n")
+        
 
 
